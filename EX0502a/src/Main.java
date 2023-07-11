252eb63605d0575a9b1137336ac50001292db7eb
Yuma Satake <satous.y@icloud.com>
2023-07-11 16:27:50 +0900

    2023/7/11

---
+++ EX0502a/src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        Sample s1 = new Sample();
+        s1.test1();
+        s1.test2();
+        s1.test1("test1");
+    }
+}
---
+++ EX0502b/src/interface_C.java
@@ -0,0 +1,3 @@
+public interface interface_C extends interface_A, interface_B {
+    public int test1(String msg);
+}
