11ef8912447db2618dbd36493769b201627d9094
Yuma Satake <satous.y@icloud.com>
2023-07-11 14:37:31 +0900

    2023/7/11-2

---
+++ EX0502/src/Sample.java
@@ -0,0 +1,13 @@
+public class Sample implements interface_A, interface_B, interface_C {
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    public void test3() {
+        System.out.println("test3");
+    }
+}
